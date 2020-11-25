77621242de65288d6aa7c802302326b00a1f0b25
satyam-srivastava1997 <satyam.srivastava771@gmail.com>
2020-11-25 17:27:32 +0000

    second

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+module "vpc"{
+source = "/home/ubuntu/newapp/modules/vpc"
+}
+
+module "ec2"{
+source = "/home/ubuntu/newapp/modules/ec2"
+subnet_id = module.vpc.subnet_id1
+subnet_id2 = module.vpc.subnet_id2
+security_group =[module.vpc.security_group]
+}
+
+
+module "elb"{
+source = "/home/ubuntu/newapp/modules/classic_elb"
+instance_id =[ module.ec2.instance_id]
+subnet_id =[ module.vpc.subnet_id1]
+subnet_id1 =[ module.vpc.subnet_id2]
+security_group = [module.vpc.security_group]
+}
